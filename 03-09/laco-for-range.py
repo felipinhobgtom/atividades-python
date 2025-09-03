46ffc655b99add371ac825b00cb69ccccfc94fc6
felipinhobgtom <felipinhobgouveia@gmail.com>
2025-09-03 09:49:24 -0300

    feat: 7, 8, 9 exercises

---
+++ 03-09/laco-for-range.py
@@ -0,0 +1,5 @@
+for num in range(0, 11, 2):
+    print(num)
+    # pode usar modulo tb
+    # if num % 2 == 0:
+    #   fazer alguma porra ...
---
+++ 03-09/while-com-lista.py
@@ -0,0 +1,6 @@
+portas = [21, 22, 23, 80]
+
+cont = 0
+while cont < len(portas):
+    print(portas[cont])
+    cont = cont + 1
